0ac7411275d358549d73b323251775b72404ad3f
Agugu95 <fkrns1744@naver.com>
2020-05-26 22:03:56 +0900

    [코드 구현: 함수를 사용하 노드 삭제]

--- src/main/java/implementation/tree/successTree.java
+++ src/main/java/implementation/tree/successTree.java
@@ -4,6 +4,7 @@ public class successTree {
     private static class Node {
         private int data;
         private Node left, right;
+
         public Node(int input) {
             this.data = input;
             this.left = null;
@@ -27,7 +28,7 @@ public class successTree {
         return this.root;
     }
 
-    public Node searchingNode(int input) {
+    public Node searchingNode(int input) { // 노드 검색
         if (root == null) {
             return null;
         }
@@ -46,8 +47,9 @@ public class successTree {
     }
 
 
-    public void addChild(int input) {
-        if (searchingNode(input) != null) {
+    public void addChild(int input) { // 노드 삽입
+        if (searchingNode(input) != null) { // 노드 값이 중복되면 삽입없이 종료
+            System.out.println("already exist in tree " + input);
             return;
         }
 
@@ -56,7 +58,7 @@ public class successTree {
         if (root == null) { // 트리가 없다면 root 생성
             root = newNode;
         } else {
-            Node moveNode = root;
+            Node moveNode = root; // 움직일 노드
             Node pointer;
 
             while (true) {
@@ -82,7 +84,93 @@ public class successTree {
 
     }
 
-    public void inOrder(Node node) {
+    public void deleteNode(int input) {
+        Node moveNode = root;
+        Node pointer = root;
+
+        boolean leftflag = true;
+
+        // 삭제 될 노드로 이동
+        while (moveNode.data != input) {
+            pointer = moveNode;
+
+            if (input < moveNode.data) {
+                leftflag = true;
+                moveNode = pointer.left;
+            } else {
+                leftflag = false;
+                moveNode = pointer.right;
+            }
+        }
+
+        Node replacementNode; // 변경될 노드
+
+        // 자식 노드가 없는 노드 삭제
+        if (moveNode.left == null && moveNode.right == null) {
+            if (moveNode == root) {
+                root = null;
+            }
+            if (leftflag) {
+                pointer.left = null;
+            } else {
+                pointer.right = null;
+            }
+        }
+
+        // 자식 노드가 한개 있는 노드 삭제
+        // 어차피 왼쪽만 있은 왼쪽만 지우면 끝
+        else if (moveNode.right == null) {
+            if (moveNode == root) {
+                root = null;
+            }
+            pointer.left = null;
+        }
+        // 오른쪽만 있다면 오른쪽만 삭제
+        else if (moveNode.left == null) {
+            if (moveNode == root) {
+                root = null;
+            }
+            pointer.right = null;
+        }
+
+        // 자식 노드가 두개일 때
+        else {
+            // 삭제될 노드의 오른쪽 트리 저장
+            Node subTree = moveNode.right;
+            // 삭제될 노드의 서브트리에서 가장 작은 노드 확인
+            replacementNode = getSubtreeLeftNode(subTree);
+            // root라면 그냥 바꿔줌
+            if (moveNode == root) {
+                root = replacementNode;
+            }
+
+            if (leftflag) {
+                pointer.left = replacementNode;
+            } else {
+                pointer.right = replacementNode;
+            }
+            replacementNode.right = subTree;
+            if (replacementNode == subTree) {
+                replacementNode.right = null;
+            }
+            replacementNode.left = moveNode.left;
+        }
+    }
+
+    private  Node getSubtreeLeftNode(Node subtree) {
+        Node pointer = subtree;
+        Node moveNode = subtree;
+
+        while (moveNode.left != null) {
+            pointer = moveNode;
+            moveNode = moveNode.left;
+        }
+        pointer.left = null;
+        return moveNode;
+    }
+
+
+    public void inOrder(Node node) { // left - root - right
         if (node != null) {
             inOrder(node.getLeft());
             System.out.println(node.data);
@@ -90,7 +178,7 @@ public class successTree {
         }
     }
 
-    public void preOrder(Node node) {
+    public void preOrder(Node node) { // root - left - right
         if (node != null) {
             System.out.println(node.data);
             preOrder(node.getLeft());
@@ -98,7 +186,7 @@ public class successTree {
         }
     }
 
-    public void postOrder(Node node) {
+    public void postOrder(Node node) { // left - right - root
         if (node != null) {
             postOrder(node.getLeft());
             postOrder(node.getRight());
@@ -113,6 +201,7 @@ public class successTree {
         tree.addChild(4);
         tree.addChild(2);
         tree.addChild(5);
+        tree.addChild(5);
         tree.addChild(9);
         tree.addChild(8);
         tree.addChild(11);
@@ -124,6 +213,8 @@ public class successTree {
         System.out.println();
         tree.postOrder(tree.getRoot());
         System.out.println();
-        System.out.println(tree.searchingNode(8));
+        tree.deleteNode(4);
+        tree.deleteNode(8);
+        tree.preOrder(tree.getRoot());
     }
 }
