40d3868385f95e9c2cb8bd9fba4363ac4cd2805d
Agugu95 <fkrns1744@naver.com>
2020-05-27 14:24:10 +0900

    [정렬: 버블 소트 구현]

---
+++ src/main/java/implementation/sorting/bubbleSort.java
@@ -0,0 +1,63 @@
+package implementation.sorting;
+
+public class bubbleSort {
+
+    public static void swap(Integer[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void simplySort(Integer[] arr) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j - 1, j);
+                }
+            }
+        }
+    }
+
+    public static void recursionSort(Integer[] a, int len) {
+        if (len <= 1) {
+            return;
+        }
+
+        boolean swapped = false;
+
+        for (int i = 1; i < len; i++) {
+            if (a[i - 1] > a[i]) {
+                swap(a, i, i - 1);
+                swapped = true;
+            }
+        }
+
+        if (swapped) {
+            recursionSort(a, len - 1);
+        }
+    }
+
+    public static void countBubble(Integer[] arr, int len) {
+        int k = 0;
+        while (k < len - 1) {
+            int last = len - 1;
+            for (int j = len - 1; j > k; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j - 1, j);
+                    last = j;
+                }
+            }
+            k = last;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{6, 4, 3, 7, 1, 9, 8, 5};
+        simplySort(arr);
+        recursionSort(arr, arr.length);
+        countBubble(arr, arr.length);
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
