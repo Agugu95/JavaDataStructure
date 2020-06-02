cbb84624c48a096aee8b9e13b6bd1966003db6d5
Agugu95 <fkrns1744@naver.com>
2020-06-02 23:57:39 +0900

    [구현: 배열 우측 피벗 퀵소트]

--- src/main/java/implementation/sorting/quick/arrayQuick.java
+++ src/main/java/implementation/sorting/quick/arrayQuick.java
@@ -1,38 +1,41 @@
 package implementation.sorting.quick;
 
 public class arrayQuick {
-    public static void quickSort(Integer[] arr, int start, int end) {
-        if (start < end) {
-            int pivot = divide(arr, start, end); // pivot 결정과 분할
-            quickSort(arr, start, pivot - 1); // data <= pivot 정렬
-            quickSort(arr, pivot + 1, end); // data >= pivot 정렬
+    public static void quickSort(Integer[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = divide(arr, left, right); // pivot 결정과 분할
+            quickSort(arr, left, pivot - 1); // data <= pivot 정렬
+            quickSort(arr, pivot + 1, right); // data >= pivot 정렬
         }
 
     }
 
-    public static int divide(Integer[] arr, int start, int end) {
-        int j = start;
-        int i = start - 1;
-        int p = arr[end];
-        while (j <= end - 1) {
-            if (arr[j] <= p) {
-                i++;
-                swap(arr, i, end);
+    public static int divide(Integer[] arr, int left, int right) {
+        int i, j, x;
+        i = left - 1;
+        j = left;
+        x = arr[right];
+        while (j <= right - 1) {
+            if (arr[j] <= x) {
+                ++i;
+                swap(arr, i, j);
             }
             j++;
         }
-        swap(arr, i + 1, end);
-        return j + 1;
+        swap(arr, i + 1, right);
+        return i + 1;
     }
+
 
     public static void swap(Integer[] arr, int a, int b) {
         Integer tmp = arr[a];
         arr[a] = arr[b];
         arr[b] = tmp;
     }
+
     public static void printArr(Integer[] arr) {
         for (Integer i : arr) {
-            System.out.println(i);
+            System.out.print(i + "\t");
         }
     }
 
