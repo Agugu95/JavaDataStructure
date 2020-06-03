027664cd7ed5f615e7afcec87ef0213ee61d8ebb
Agugu95 <fkrns1744@naver.com>
2020-06-03 17:38:26 +0900

    [구현: 우측 피벗 오름차순 내림차순]

--- src/main/java/implementation/sorting/quick/rightQuick.java
+++ src/main/java/implementation/sorting/quick/rightQuick.java
@@ -3,20 +3,36 @@ package implementation.sorting.quick;
 public class rightQuick {
     public static void quickSort(Integer[] arr, int left, int right) {
         if (left < right) {
-            int pivot = divide(arr, left, right); // pivot 결정과 분할
+            int pivot = ascPartition(arr, left, right); // pivot 결정과 분할
             quickSort(arr, left, pivot - 1); // data <= pivot 정렬
             quickSort(arr, pivot + 1, right); // data >= pivot 정렬
         }
 
     }
 
-    public static int divide(Integer[] arr, int left, int right) {
+    public static int ascPartition(Integer[] arr, int left, int right) {
         int i, j, x;
         i = left - 1; // -1
         j = left; // 0
         x = arr[right]; // pivot
         while (j <= right - 1) { // pivot - 1까지 순회
             if (arr[j] <= x) { // pivot보다 작은 값이면
+                ++i; // i 증가 시키고 스왑
+                swap(arr, i, j);
+            }
+            j++; // j 증가
+        }
+        swap(arr, i + 1, right); // pivot보다 작은 값만 앞에 남았으니 스왑
+        return i + 1; // 새 pivot 결정
+    }
+
+    public static int descPartition(Integer[] arr, int left, int right) {
+        int i, j, x;
+        i = left - 1; // -1
+        j = left; // 0
+        x = arr[right]; // pivot
+        while (j <= right - 1) { // pivot - 1까지 순회
+            if (arr[j] >= x) { // pivot보다 작은 값이면
                 ++i; // i 증가 시키고 스왑
                 swap(arr, i, j);
             }
