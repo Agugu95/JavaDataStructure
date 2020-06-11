726126d4479815aa2de88dde975684ebb2cfe014
Agugu95 <fkrns1744@naver.com>
2020-06-11 23:47:01 +0900

    [주석: 백준]

--- src/main/java/implementation/boj/maxHeap_11279.java
+++ src/main/java/implementation/boj/maxHeap_11279.java
@@ -1,26 +1,25 @@
-package implementation.heap;
+package implementation.boj;
 
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class priorityQueue {
+public class maxHeap_11279 {
+    // 최대 힙을 통해 0이면 데이터 출력
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-        int data = 0;
+        int data;
         if (input != 0 && input < 100001) {
             PriorityQueue<Integer> pq = new PriorityQueue<>(1000000, Collections.reverseOrder());
             while (input != 0) {
                 data = sc.nextInt();
                 if (data != 0) {
                     pq.add(data);
+                } else if (pq.peek() == null) {
+                    System.out.println(0);
                 } else {
-                    if (pq.peek() == null) {
-                        System.out.println(0);
-                    } else {
-                        System.out.println(pq.poll());
-                    }
+                    System.out.println(pq.poll());
                 }
                 input--;
             }
