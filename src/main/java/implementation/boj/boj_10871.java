4b73d343a98fbc3b05b7a93595f68e132848feb2
Agugu95 <fkrns1744@naver.com>
2020-06-11 23:40:32 +0900

    [주석: 백준]

--- src/main/java/implementation/boj/boj_10871.java
+++ src/main/java/implementation/boj/boj_10871.java
@@ -3,6 +3,7 @@ package implementation.boj;
 import java.util.Scanner;
 
 public class boj_10871 {
+    // 1보다 작은 수, X보다 작은 수 출력
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
